a13c9d5d6b97d24c314b58b8f64c004f41a39147
Rod Schmidt <rod@infinitenil.com>
2026-08-31 11:35:17 -0600

    Initial Commit

---
+++ SwishEmbed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwishEmbed
+//
+//  Created by Rod Schmidt on 8/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwishEmbed/SwishEmbedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwishEmbedApp.swift
+//  SwishEmbed
+//
+//  Created by Rod Schmidt on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct SwishEmbedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
